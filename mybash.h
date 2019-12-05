35ed1c60f698e565a767c59f4c81a8750f4f2856
Abijeet123 <abijeetshyamm@gmail.com>
2019-12-05 00:52:09 -0500

    first commit

---
+++ mybash.h
@@ -0,0 +1,13 @@
+#define IF if(
+#define THEN ){
+#define FI }
+#define START void main(){
+#define END }
+#define INT int
+#define PTR void *
+#include <stdio.h> 
+#define ELSE else {
+#define STRING char *
+#define WHILE ;while (
+#define DO ){
+#define OD }
