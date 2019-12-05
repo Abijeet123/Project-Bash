34d479d04dfcd43ea39846ba404cd84b21b093c7
Abijeet123 <abijeetshyamm@gmail.com>
2019-12-05 16:05:52 +0530

    Added a custom print function and some more dependencies

--- mybash.h
+++ mybash.h
@@ -1,13 +1,23 @@
 #define IF if(
 #define THEN ){
 #define FI }
-#define START void main(){
+#define START {
+#define BEGIN void main(){
 #define END }
 #define INT int
 #define PTR void *
 #include <stdio.h> 
 #define ELSE else {
 #define STRING char *
-#define WHILE ;while (
+#define WHILE while (
 #define DO ){
 #define OD }
+#define TYPE typedef
+#define FLOAT float 
+#define CHAR char
+#define DOUBLE double
+#define OR ||
+#define AND &&
+#define NOT !
+#define EQ ==
+#include "print.h"
